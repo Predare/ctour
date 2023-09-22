f855c2c15ef6d7f9a4d78b812f67b9f0145fcf80
Danil Kassymov <predare12000@gmail.com>
2023-09-22 08:09:43 +0300

    All social buttons merged in one

--- components/Film/FilmPlayerWidget.vue
+++ components/Film/FilmPlayerWidget.vue
@@ -15,12 +15,19 @@
         </div>
         <div class="m-0 mt-2 opacity-60 text-subtitle-1">Понравилось? Поделитесь!</div>
         <div class="flex flex-row gap-3 justify-center">
-            <SocialButtonsVK/>
-            <SocialButtonsFacebook/>
-            <SocialButtonsOK/>
-            <SocialButtonsTelegram/>
-            <SocialButtonsTwitter/>
-            <SocialButtonsMoiMir/>
+            <SocialButton link='https://vkontakte.ru/share.php?url=http://localhost:3000/film/fies'
+                icon="fa:fas fa-brands fa-vk" tooltipText="ВКонтакте" />
+            <SocialButton link='https://www.facebook.com/sharer.php?u=http://localhost:3000/film/fies'
+                icon="fa:fas fa-brands fa-facebook" tooltipText="Facebook" />
+            <SocialButton
+                link='https://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=http://localhost:3000/film/fies&st.comments=ОПИСАНИЕ"'
+                icon="fa:fas fa-brands fa-odnoklassniki" tooltipText="Одноклассники" />
+            <SocialButton link='https://telegram.me/share/url?url=http://localhost:3000/film/fies"'
+                icon="fa:fas fa-brands fa-telegram" tooltipText="Телеграм" />
+            <SocialButton link='https://twitter.com/intent/tweet?text=Title. Description. http://localhost:3000/film/fies"'
+                icon="fa:fas fa-brands fa-twitter" tooltipText="Twitter" />
+            <SocialButton link='https://connect.mail.ru/share?url=http://localhost:3000/film/fies"' icon="mdi-share"
+                tooltipText="Мой мир" />
         </div>
     </div>
 </template>
---
+++ components/SocialButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+    tooltipText: String,
+    icon: String,
+    link: String,
+})
+function openSharePage() {
+    window.open(props.link, "sharer", "toolbar=0,status=0,width=626,height=436");
+}
+</script>
+
+<template>
+    <div>
+        <v-btn :icon="icon" @click="openSharePage" variant="plain" density="compact" size="small" :ripple="false">
+        </v-btn>
+        <v-tooltip activator="parent" location="bottom" :text="tooltipText"> </v-tooltip>
+    </div>
+</template>
