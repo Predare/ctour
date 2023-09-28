a4fdb9825d9b142244ec2d4930f7155d3795ba27
Danil Kassymov <predare12000@gmail.com>
2023-09-28 06:04:16 +0300

    refactoring create film API

--- server/api/admin/film/create.post.js
+++ server/api/admin/film/create.post.js
@@ -2,7 +2,7 @@ import textTranslitServer from '~/composables/textTranslit.server';
 import { db } from '~/utils/db.server';
 
 export default defineEventHandler(async (event) => {
-  const { serverMeilisearchClient } = event.context
+  const { serverMeilisearchClient } = event.context;
   const body = await readBody(event);
   var result = null;
 
@@ -14,58 +14,58 @@ export default defineEventHandler(async (event) => {
     })
   });
 
-  async function addFilm() {
-    result = await db.film.create({
-      data: {
-        type: body.type,
-        name: body.name,
-        description: body.description,
-        genres: {
-          connect: genreObjects,
-        },
-        yearStart: Number(body.yearStart),
-        yearEnd: Number(body.yearEnd),
-        link: textTranslitServer(body.name + ' ' + body.yearStart),
-        views: 0,
-      }
-    })
-  }
-
-  async function findExistedGenres() {
-    return await db.genre.findMany({
-      where: {
-        name: {
-          in: body.genres.map(genre => {
-            return genre.title;
-          })
-        }
-      }
-    });
-  }
-
-  const existedGenres = await findExistedGenres();
+  const existedGenres = await findExistedGenres(body.genres);
   const newGenres = body.genres.filter(genre => {
     return existedGenres.find(existedGenre => existedGenre.name === genre.title) == undefined;
-  })
+  });
 
-  async function addNewGenres() {
-    await db.genre.createMany({
-      data: newGenres.map(genre => {
-        return {
-          name: genre.title
-        }
-      })
-    });
-    var result = await serverMeilisearchClient.index('genres').addDocuments(newGenres.map(genre => {
+  if (newGenres.length > 0) await addNewGenres(newGenres, serverMeilisearchClient);
+  await addFilm(body.type, body.name, body.description, body.genres, body.yearStart, body.yearEnd);
+
+  return {};
+})
+
+async function addFilm(type, name, description, genres, yearStart, yearEnd) {
+  result = await db.film.create({
+    data: {
+      type: type,
+      name: name,
+      description: description,
+      genres: {
+        connect: genres,
+      },
+      yearStart: Number(yearStart),
+      yearEnd: Number(yearEnd),
+      link: textTranslitServer(name + ' ' + yearStart),
+      views: 0,
+    }
+  })
+}
+
+async function findExistedGenres(genres) {
+  return await db.genre.findMany({
+    where: {
+      name: {
+        in: genres.map(genre => {
+          return genre.title;
+        })
+      }
+    }
+  });
+}
+
+async function addNewGenres(genres, serverMeilisearchClient) {
+  await db.genre.createMany({
+    data: genres.map(genre => {
       return {
-        id: 11,
         name: genre.title
       }
-    }));
-  }
-  if(newGenres.length > 0)await addNewGenres();
-  await addFilm();
-  return {
-
-  };
-})
+    })
+  });
+  var result = await serverMeilisearchClient.index('genres').addDocuments(genres.map(genre => {
+    return {
+      id: 11,
+      name: genre.title
+    }
+  }));
+}
